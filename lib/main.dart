318af4f092f717cd13433fe34f287368cf01ff30
ThairongSeng <seng.thairong@gmail.com>
2023-11-23 15:27:31 +0700

    correct the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
+
 import 'package:flutter/material.dart';
 import 'package:food_panda_flutter_ui_app/views/pages/home_page.dart';
-import 'package:food_panda_flutter_ui_app/views/pages/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,19 +19,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor:  Colors.grey[200],
         useMaterial3: true,
       ),
-        home: AnimatedSplashScreen(
-            duration: 3000,
-            splash: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Image.asset("assets/images/welcome.png"),
-                Image.asset("assets/images/becomepro.png")
-              ],
-            ),
-            splashIconSize: double.maxFinite,
-            nextScreen: const HomePage(),
-            backgroundColor: Colors.pinkAccent),
+        home: const HomePage()
     );
   }
 }
